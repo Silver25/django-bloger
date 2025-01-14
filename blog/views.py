aa27190859bb2ac6dc6626625d82a08c75f3616f
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-14 18:07:15 +0000

    Remove extra code-comented code to clean files

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-# from django.http import HttpResponse - remove for templates
 from django.views import generic
 from .models import Post
 
 # Create your views here.
-# def blog(request): - remove for templates
-#     return HttpResponse("Hello, Blog!")
 class PostList(generic.ListView):
-    # model = Post - remove couse redundant by the queryset
-    # use only published records from the Post model & display in descending order
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "post_list.html"
