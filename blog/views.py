f988e565ec8ce99746669ca7460c1d1aa4a8b0da
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-14 17:19:29 +0000

    Change Posts view filter for only published posts

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,6 @@ from .models import Post
 #     return HttpResponse("Hello, Blog!")
 class PostList(generic.ListView):
     # model = Post - remove couse redundant by the queryset
-    # use all of the records from the Post model & display in descending order
-    queryset = Post.objects.all().order_by("-created_on")
+    # use only published records from the Post model & display in descending order
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "post_list.html"
