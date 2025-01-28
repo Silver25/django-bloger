5c4023271c6e6d3b997f92cea9a0f5d92dec3fc9
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-28 13:51:49 +0000

    Add about variable and link to about page in base.html

--- about/views.py
+++ about/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import About
 
 # Create your views here.
-def about_me(request):
+def about_all(request):
     """
     Renders the About page
     """
